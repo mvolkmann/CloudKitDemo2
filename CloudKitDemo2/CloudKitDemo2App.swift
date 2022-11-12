0aa709e70fa49ea1a563ced4968c573975960743
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-11-12 11:27:49 -0600

    Initial Commit

---
+++ CloudKitDemo2/CloudKitDemo2App.swift
@@ -0,0 +1,17 @@
+//
+//  CloudKitDemo2App.swift
+//  CloudKitDemo2
+//
+//  Created by Mark Volkmann on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CloudKitDemo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
